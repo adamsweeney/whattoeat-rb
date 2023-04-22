26813b2b026a7bbeae7c208878413c9cb29fc67d
Adam Sweeney <adam.a.sweeney@gmail.com>
2023-04-22 15:56:37 -0400

    Add a total_pages within the meta data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::API
 
   def metadata(object)
+    per_page = 15
     {
       current_page: object.current_page,
       total_entries: object.total_entries,
-      per_page: 15
+      total_pages: (object.total_entries / per_page.to_f).ceil,
+      per_page: per_page
     }
   end
 end
